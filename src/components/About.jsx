3cca98112d9bc05d02dd55d49f0bbd058a712f0f
Sufiyan2661 <mohdsufiyan99770@gmail.com>
2024-10-21 13:32:35 +0530

    changes made in about comoponent

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,9 +6,11 @@ import { motion } from "framer-motion";
 const About = () => {
   return (
     <div className="border-b flex flex-col justify-center border-neutral-900  pb-4 px-1">
-      <h1 className="my-20 text-center text-3xl">
+      <motion.h1 whileInView={{opacity: 1,y:0}}
+      initial={{opacity:0,y: -100}}
+      transition={{duration:0.5}} className="my-20 text-center text-3xl">
         About <span className="text-neutral-500">Me</span>
-      </h1>
+      </motion.h1>
       <div className="flex flex-wrap">
         <motion.div
           whileInView={{ opacity: 1, x: 0 }}
